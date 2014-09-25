8123616689cbaf5dd0f3e2b955a90c5e81419dd0
RobbieL811 <robbielancaster811@gmail.com>
2014-09-25 01:28:55 -0400

    Fix cameranext lib

--- libs/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := libjni_mosaic
-LOCAL_SRC_FILES := libjni_mosaic.so
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_PREBUILT)
